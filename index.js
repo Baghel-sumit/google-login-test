0d4cfcfb0dbc58a83f435ee0207003a46fcc6db8
Sumit123bahgel <sarvgt435216@gmail.com>
2023-02-19 15:02:45 +0530

    fix: added response from google

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const client_id = "299371230166-84u5qlbm0j2fkntbm8vouqj5nc250k4q.apps.googleusercontent.com";
 
+function parseJwt (token) {
+  var base64Url = token.split('.')[1];
+  var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+  var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function(c) {
+      return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+  }).join(''));
+
+  return JSON.parse(jsonPayload);
+}
+
+
 function handleCredentialResponse(response) {
-    console.log("hey i am executed");
-    console.log("Encoded JWT ID token: " + response.credential);
-    console.log("response from google:", response);
+    const token =  response.credential;
+    console.log("Encoded JWT ID token: " + token);
+    console.log("response from google:", parseJwt(token))
 }
 window.onload = function () {
     console.log("hey i am here");
